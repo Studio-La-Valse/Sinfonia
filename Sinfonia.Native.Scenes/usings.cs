c2a6772bfc3e9d2a4cb6b51760077fda49e90e3a
Roel Westrik <roelwestrik@gmail.com>
2024-04-14 10:14:26 +0200

    oops

--- Sinfonia.Native.Scenes/usings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Sinfonia.API;
-global using StudioLaValse.Drawable.ContentWrappers;
-global using StudioLaValse.Geometry;
-global using StudioLaValse.ScoreDocument;
-global using StudioLaValse.ScoreDocument.Core.Primitives;
-global using StudioLaValse.ScoreDocument.Drawable.PageView.ScoreElements;
